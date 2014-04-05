e0c033551f2d6ac0d38e495b5367c0f5b28122a0
Taimur <taimur38@gmail.com>
2014-04-05 17:55:39 -0400

    page fix

--- sink/Crawlers/LasftFmCrawler.cs
+++ sink/Crawlers/LasftFmCrawler.cs
@@ -22,7 +22,7 @@ namespace sink.Crawlers
         public override int Crawl()
         {
             var wc = new WebClient();
-            var page = 0;
+            var page = 1;
 
             var items_crawled = 0;
 
