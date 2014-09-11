b77558df254c9ce2d652ed28ed207a8b3ebb824f
Taimur <taimur38@gmail.com>
2014-09-10 22:50:26 -0500

    one endpoint for state receiving

--- sink/Modules/ReceiverModule.cs
+++ sink/Modules/ReceiverModule.cs
@@ -24,86 +24,30 @@ namespace sink.Modules
 
             Post["/state"] = parameters =>
                 {
-                    using(var reader = new StreamReader(this.Request.Body))
+                    try
                     {
-                        var json = reader.ReadToEnd();
+                        using (var reader = new StreamReader(this.Request.Body))
+                        {
+                            var json = reader.ReadToEnd();
 
-                        var obj = JObject.Parse(json);
-                        var t = Type.GetType("sink.States." + obj["Type"]);
+                            var obj = JObject.Parse(json);
 
-                        var typed = (BaseState)obj.ToObject(t);
-                        Console.WriteLine(typed.GetType());
-                        //typed.Save();
-                        
-                        
-                        Console.WriteLine(json);
-                        return json;
+                            var t = Type.GetType("sink.States." + obj["Type"]);
+                            var typed = (BaseState)obj.ToObject(t);             //catch errors on input
+
+                            typed.Save();
+
+                            Console.WriteLine(json);
+                            return json;
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        return e.Message;
                     }
                     
-                };
-
-            Post["/app"] = parameters =>
-            {
-                try
-                {
-                    var entry = this.Bind<AppState>(config, f => f.Id);
-
-                    entry.Save();
-
-                    return "ok";
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    return e.Message;
-                }
-            };
-
-            Post["/location"] = parameters =>
-                {
-                    try
-                    {
-                        var location = this.Bind<LocationState>(config, f => f.Id);
-                        location.Save();
-                        return "ok";
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                        return e.Message;
-                    }
-                };
-
-            Post["/battery"] = parameters =>
-                {
-                    try
-                    {
-                        var battery = this.Bind<BatteryState>(config, f => f.Id);
-                        battery.Save();
-
-                        return "ok";
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                        return e.Message;
-                    }
-                };
-
-            Post["/conn"] = parameters =>
-                {
-                    try
-                    {
-                        var conn = this.Bind<ConnectivityState>(config, f => f.Id);
-                        conn.Save();
-
-                        return "ok";
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                        return e.Message;
-                    }
                 };
         }
     }
