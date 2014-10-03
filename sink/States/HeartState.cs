4efd9eb49758e0856fab894bf5c5c6d9d99998a5
Taimur <taimur38@gmail.com>
2014-10-02 19:01:50 -0500

    added heartstate

---
+++ sink/States/HeartState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sink.States
+{
+    public class HeartState : BaseState
+    {
+        public HeartState()
+        {
+
+        }
+
+        public double BPM { get; set; }
+
+        public override void Save()
+        {
+            this.Collection<HeartState>().Save(this);
+        }
+    }
+}
